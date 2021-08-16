dc5a2373a14f0b473a818dcaf98ddcbf86678fcd
Rizqi Pratama <rev4rmy@gmail.com>
2021-08-16 13:07:44 +0700

    Update Key

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ const apiShortener = {
   url: "https://pub-metranet-api.thebigbox.id/URL-Shortener/1.0.0/shortener",
   config: {
     headers: {
-      "x-api-key": "8yIz83V26FIsiXwsDQ5zdfpkOqoQHaCq",
+      "x-api-key": "CBZ8H8Zfj4kVE4NMaF3WssKxIdvy4KGD",
       "Content-Type": "application/x-www-form-urlencoded",
     },
   },
@@ -12,7 +12,7 @@ const apiSMS = {
   url: "https://api.thebigbox.id/sms-notification/2.0.0/messages",
   config: {
     headers: {
-      "x-api-key": "8yIz83V26FIsiXwsDQ5zdfpkOqoQHaCq",
+      "x-api-key": "CBZ8H8Zfj4kVE4NMaF3WssKxIdvy4KGD",
       "Content-Type": "application/x-www-form-urlencoded",
     },
   },
